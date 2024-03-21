6ee3718c90709cba82f77cb3474d9e759ad468c6
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-21 19:52:30 -0300

    feat: pass info as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import * as info from "./info.json";
+import Info from "./types/info";
+interface AppProps {
+    info: Info;
+}
 
-function App() {
+function App({ info }: AppProps) {
     return (
         <section className="flex flex-col items-center w-full pt-36">
             <div className="text-4xl">{info.name}asdasd</div>
