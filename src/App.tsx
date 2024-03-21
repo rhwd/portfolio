76102bd4dea07761731e271a9f9f81212811036f
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-21 00:52:04 -0300

    feat: start app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import * as info from "./info.json";
 
-  return (
-    <>
-    </>
-  )
+function App() {
+    return (
+        <section className="flex flex-col items-center w-full pt-36">
+            <div className="text-4xl">{info.name}asdasd</div>
+            <div className="text-lg">{info.role}</div>
+            {info.stack.map((tech, idx) => (
+                <img className="h-4 w-5" key={idx} src={tech.imagePath} />
+            ))}
+        </section>
+    );
 }
 
-export default App
+export default App;
