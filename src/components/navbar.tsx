f647d360ef8c94ca8d87c4e755853bf54d64176a
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-22 01:08:35 -0300

    feat: open navbar links in new tab

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,11 +12,17 @@ function Navbar({ socials }: NavbarProps) {
     return (
         <nav className="flex flex-row items-center justify-end py-4 space-x-4 px-8">
             {socials.map((social) => (
-                <a href={social.url}>
-                    <img className="h-8" src={social.imagePath} />
-                </a>
+                <img
+                    className="h-8"
+                    src={social.imagePath}
+                    onClick={() => openInNewTab(social.url)}
+                />
             ))}
         </nav>
     );
 }
+const openInNewTab = (url: string): void => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+};
 export default Navbar;
