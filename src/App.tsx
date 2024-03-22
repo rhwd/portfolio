e078510a07726278555f776fe5c2789fc9d58a61
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-22 01:00:38 -0300

    feat: componentize stack showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/navbar";
+import StackShowcase from "./components/stack";
 import Info from "./types/info";
 interface AppProps {
     info: Info;
@@ -9,17 +10,9 @@ function App({ info }: AppProps) {
         <>
             <Navbar socials={info.socials} />
             <section className="flex flex-col items-center w-full pt-36">
-                <div className="text-4xl">{info.name}</div>
-                <div className="text-lg ">{info.role}</div>
-                <div className="flex flex-row space-x-4">
-                    {info.stack.map((tech, idx) => (
-                        <img
-                            className="h-8 w-8"
-                            key={idx}
-                            src={tech.imagePath}
-                        />
-                    ))}
-                </div>
+                <div className="text-4xl md:text-8xl">{info.name}</div>
+                <div className="text-lg md:text-3xl mb-8">{info.role}</div>
+                <StackShowcase stack={info.stack} />
             </section>
         </>
     );
