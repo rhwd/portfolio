790f5edc8df84b4185692f45b25b97048e27c268
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-23 23:12:10 -0300

    feat: improve responsivity

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App({ info }: AppProps) {
     return (
         <>
             <Navbar socials={info.socials} />
-            <section className="flex flex-col items-center w-full pt-36">
-                <div className="text-4xl md:text-8xl">{info.name}</div>
+            <section className="flex flex-col items-center w-full pt-16 2xl:pt-36">
+                <div className="text-5xl 2xl:text-8xl">{info.name}</div>
                 <div className="text-lg md:text-3xl mb-8">{info.role}</div>
                 <StackShowcase stack={info.stack} />
                 <text className="mt-16 md:w-1/3 mx-6 text-center text-md sm:text-xl">
